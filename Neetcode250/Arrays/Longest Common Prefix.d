ef0c86e59565abedebdf941e2f998969a4bdac46
d.j. <87750461+DebjitUrinda@users.noreply.github.com>
2026-04-07 11:58:33 +0530

    Add problem statement for longest common prefix
    Added problem statement for longest common prefix challenge.

---
+++ Neetcode250/Arrays/Longest Common Prefix.d
@@ -0,0 +1,3 @@
+### Problem Statement (https://neetcode.io/problems/longest-common-prefix/question?list=neetcode250):
+  You are given an array of strings strs. Return the longest common prefix of all the strings.
+  If there is no longest common prefix, return an empty string "".
